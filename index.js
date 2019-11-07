35984a1e006f9850dc81de777796675f0050467c
thistonyb <this.tonybaker@gmail.com>
2019-11-06 16:13:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let returnedArray = [];
+  sourceArray.forEach(function(element) {
+    returnedArray.push(element * -1);
+  });
+  return returnedArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let returnedArray = [];
+  sourceArray.forEach(function(element) {
+    returnedArray.push(element);
+  });
+  return returnedArray;
+}
+
+function mapToDouble(sourceArray) {
+  let returnedArray = [];
+  sourceArray.forEach(function(element) {
+    returnedArray.push(element * 2);
+  });
+  return returnedArray;
+}
+
+function mapToSquare(sourceArray) {
+  let returnedArray = [];
+  sourceArray.forEach(function(element) {
+    returnedArray.push(element * element);
+  });
+  return returnedArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+  let total = startingPoint || 0;
+  sourceArray.forEach(function(element) {
+    total += element;
+  });
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let flag = true;
+  sourceArray.forEach(function(element) {
+    if (element == false) {
+      flag = false;
+    }
+  });
+  return flag;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let flag = false;
+  sourceArray.forEach(function(element) {
+    if (element == true) {
+      flag = true;
+    }
+  });
+  return flag;
+}
